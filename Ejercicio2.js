f46ef19dc17230670f927979dc6537d6d333c115
Daniela Avila Reyes <aviladanielap@gmail.com>
2022-09-06 15:19:28 -0300

    ejercicio 2

---
+++ Ejercicio2.js
@@ -0,0 +1,14 @@
+const nombre = "Daniela";
+
+const edad = 25;
+
+const dev = true;
+
+const cumpleños = new Date (1997, 2, 12);
+
+const librofavorito = {
+    titulo: "Ante todo no hagas daño",
+    autor: "Henry Marsh",
+    year: 2010,  
+    editorial: planeta,
+}

4a5c4048ab6c5fa7add797964c9b56893c2e89a7
Daniela Avila Reyes <aviladanielap@gmail.com>
2022-09-10 10:50:40 -0300

    ejercicio 4

---
+++ ejercicio4.js
@@ -0,0 +1,20 @@
+let nombre = "Daniela";
+let apellido = "Avila";
+
+let estudiante = ` ${nombre} ${apellido} `;
+console.log(estudiante);
+
+let estudianteMayus = console.log(estudiante.toUpperCase());
+
+let estudianteMinus = console.log(estudiante.toLowerCase());
+
+let logitudEstudiante = console.log(estudiante.length);
+
+let letraNombre = console.log(nombre[0]);
+
+let letraApellido = console.log(apellido[0]);
+
+let eliminaEspacio = console.log(estudiante.trim().length);
+
+let nombreEnEstudiante = console.log(estudiante.includes("Daniela"));
+
